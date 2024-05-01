6767c206ef31e4c170ab2d930a4e2cb74dd6dfdb
Afgan Prasetyawan <98010655+Afgan1@users.noreply.github.com>
2024-05-01 16:33:21 +0700

    Add files via upload
    Add main file

---
+++ script.js
@@ -0,0 +1,92 @@
+let items = JSON.parse(localStorage.getItem("items"))
+if(!items){
+  items = []
+}
+//console.log(items)
+const list = document.querySelector(".list")
+const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+const day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+const activity = ["Click for edit", "Work", "Study", "Buy a coffe"]
+
+const date = new Date()
+document.querySelector(".today").textContent = `${day[date.getDay()]}, ${date.getDate()} ${month[date.getMonth()]} ${date.getFullYear()}`
+
+document.querySelector(".years").textContent = String(date.getFullYear())
+
+function add(){
+  save()
+  items = items.filter(el => el).map((el, i) => el.replace(/(?<=item\s)\d*/, i))
+  list.innerHTML = items.reduce((acc, cur) => acc + "\n" + cur, "")
+  //console.log(array)
+}
+add(items)
+function update(target){
+  const parent = target.parentElement
+  //console.log(parent.classList[1])
+  items[parent.classList[1]] = parent.outerHTML
+  save()
+}
+
+function save(){
+  localStorage.setItem("items", JSON.stringify(items))
+}
+
+function deleted(target){
+  delete items[target.parentElement.classList[1]]
+  add(items)
+  //console.log("delete", target.parentElement)
+}
+
+document.addEventListener("click", function(e){
+  const array = [...e.target.classList]
+  const target = e.target
+  if(array.includes("checkbox")){
+    //e.target.nextSibling.nextSibling.style.textDecoration = e.target.checked ? "line-through" : "none"
+    
+    target.previousSibling.previousSibling.classList.toggle("checked")
+    if([...target.classList].includes("active")){
+      //console.log(target.previuosSibling.previuosSibling.classList)
+      //add(items)
+    }
+    update(target)
+  }
+  if(array.includes("add")){
+    add(items)
+    const i = items.length
+    const item =
+`<div class="item ${i}">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 container--delete delete">
+    <path fill-rule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+    </svg>
+
+  <div class="text" contenteditable="true">${activity[items.length] || activity[0]}</div>
+  <svg class="checkbox" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+   <path fill-rule="evenodd" d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z" clip-rule="evenodd" />
+  </svg>
+</div>`
+    items.push(item)
+    add(items)
+    //console.log(items.length)
+  }
+  if(array.includes("delete")){
+    deleted(target)
+  }
+  if(array.includes("save")){
+    save()
+    alert("Saved items")
+  }
+  if(array.includes("clear")){
+    items = []
+    add(items)
+  }
+  //console.log(target)
+})
+
+document.addEventListener("input", function(e){
+  const target = e.target
+  const array =  [...target.classList]
+  if(array.includes("text")){
+    update(target)
+  }
+})
+
---
+++ theme.js
@@ -0,0 +1,56 @@
+let template = JSON.parse(localStorage.getItem("template"))
+if(!template){
+  saveTheme("white", "blue")
+  template = JSON.parse(localStorage.getItem("template"))
+}
+let [background, color] = template
+
+document.body.style.backgroundColor = background
+document.body.style.color = color
+
+function saveTheme(background, color){
+  localStorage.setItem("template", JSON.stringify([background, color]))
+}
+
+const templates = [
+  ["black", "white"],
+  ["white", "black"],
+  ["yellow", "blue"],
+  ["pink", "black"],
+  ["red", "white"],
+  ["red", "yellow"],
+  ["white", "blue"],
+  ["blue", "white"],
+  ["pink", "white"]
+]
+
+let index = 0
+
+document.addEventListener("click", function(e){
+  const target = e.target
+  const classes = [...target.classList]
+  if(classes.includes("template")){
+    changeTheme()
+  }
+})
+
+document.addEventListener("change", function(e){
+  const target = e.target
+  const color = target.value
+  if(target.id === "background"){
+    document.body.style.backgroundColor = color
+  }
+  if(target.id === "color"){
+    document.body.style.color = color
+  }
+})
+
+function changeTheme(){
+  const template = templates[index]
+  background = template[0]
+  color = template[1]
+  document.body.style.backgroundColor = background
+  document.body.style.color = color
+  index = (index + 1) % templates.length
+  saveTheme(background, color)
+}
